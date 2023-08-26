7718341215dbb499b003796c0a8e95185ba519c3
JannatunNimeNishat <nishatnime100@gmail.com>
2023-08-26 16:45:59 +0600

    NFT row added

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,44 @@ import nFTify_logo from './assets/left_side_bar/NFTify.png';
 import content_bg from './assets/contenet_area/content_bg.png';
 import searchIcon from './assets/contenet_area/searchIcon.png';
 import axios from 'axios';
+import NFTrow from './components/NFTrow';
 function App() {
   const [activeLink, setActiveLink] = useState(1);
   const [nftData, setNftData] = useState([]);
+
   const handleTokenAddress = (active, value) => {
-
     setActiveLink(active);
-    axios
-
+    setNftData([]);
+    axios.get(`https://api.dexscreener.com/latest/dex/tokens/0x2170Ed0880ac9A755fd29B2688956BD959F933F8`)
+      .then(res => setNftData(res.data.pairs))
+      .catch(error => console.log(error))
 
   }
 
   const handlePairAddress = (active, value) => {
 
     setActiveLink(active);
+    setNftData([]);
+    axios.get(`https://api.dexscreener.com/latest/dex/pairs/bsc/0x7213a321F1855CF1779f42c0CD85d3D95291D34C`)
+      .then(res => setNftData(res.data.pairs))
+      .catch(error => console.log(error))
 
   }
+  console.log(nftData);
   return (
     <>
       <div className="drawer lg:drawer-open">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle  " />
         <div className="drawer-content flex flex-col items-center justify-center">
           {/* Page content here */}
 
 
           {/* nft data container */}
-          <div className={`w-full h-screen bg-cover bg-center`} style={{ backgroundImage: `url(${content_bg})` }}>
+          <div className={`w-full   bg-cover bg-center`} style={{ backgroundImage: `url(${content_bg})` }}>
 
-            <div className="w-full h-full  backdrop-brightness-50">
+            <div className="w-full h-full py-5  backdrop-brightness-50">
               {/* nav */}
-              <div className='flex justify-between items-center pt-[27px] px-[65px]'>
+              <div className='flex justify-between items-center pt-[20px] px-[65px]'>
                 {/* search */}
                 <div className="form-control">
                   <div className="input-group ">
@@ -53,17 +61,35 @@ function App() {
               {/* token */}
               <div className={`${activeLink === 1 ? '' : 'hidden'}  px-10 mt-10   `}>
                 <h3 className='text-white text-[24px] font-semibold'>Token Search Results</h3>
-
+                {/* cards */}
+                <div>
+                  {
+                    nftData.slice(0, 2).map((nftSinglePair, index) => <NFTrow
+                      key={index}
+                      nftSinglePair={nftSinglePair}
+                    />)
+                  }
+                </div>
 
 
 
               </div>
+              {/* pair  */}
+              <div className={`${activeLink === 2 ? '' : 'hidden'}  px-10 mt-10   `}>
+                <h3 className='text-white text-[24px] font-semibold'>Token Search Results</h3>
+                {/* cards */}
+                <div>
+                  {
+                    nftData.slice(0, 2).map((nftSinglePair, index) => <NFTrow
+                      key={index}
+                      nftSinglePair={nftSinglePair}
+                    />)
+                  }
+                </div>
 
 
 
-
-
-
+              </div>
 
             </div>
 
@@ -78,7 +104,7 @@ function App() {
           <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
         </div>
-        <div className="drawer-side">
+        <div className="drawer-side ">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
           <ul className="menu py-4 w-80 min-h-full bg-[#292929] text-white">
             {/* Sidebar content here */}
---
+++ src/components/NFTrow.jsx
@@ -0,0 +1,65 @@
+
+import baseInfo_btn_bg from '../assets/card/card_bg.png'
+import baseInfo_icon from '../assets/card/baseInfo_icon.png'
+import material_symbols from '../assets/card/material-symbols_token-outline.png'
+
+const NFTrow = ({ nftSinglePair }) => {
+    console.log(nftSinglePair);
+   
+    const { pairCreatedAt, dexId, pairAddress, baseToken, quoteToken, priceNative, priceUsd } = nftSinglePair;
+    console.log(baseToken);
+    return (
+        <div className='mt-[22px] flex gap-3'>
+            {/* basic info */}
+            <div className=" relative w-[230px] h-[220px]  bg-[#390554] rounded-[10px] text-white px-5 py-4">
+                <h3 className="font-bold text-[18px]">Basic Info</h3>
+                <p className="pt-2">Pair created at <span className="ml-3">{baseToken?.name?.split(" ")[0]}</span></p>
+                <p className="pt-1">Symbol <span className="ml-16">{baseToken?.symbol}</span></p>
+                <p className="w-full  pt-1 ">Dex ID <span className=" ml-16">#{dexId.slice(0, 3)}</span></p>
+                <p className="w-full  pt-1 ">Pair Address <span className="ml-7   ">#{pairAddress.slice(0, 4)}</span></p>
+                <button className='absolute right-2 bottom-5 '>
+                    <img src={baseInfo_btn_bg} alt="" />
+                    <img className='ml-4 -mt-10' src={baseInfo_icon} alt="" />
+                </button>
+            </div>
+
+            {/* base token */}
+            <div className=" relative w-[230px] h-[220px]  bg-[#390554] rounded-[10px] text-white px-5 py-4">
+                <h3 className="font-bold text-[18px]">Basic Token</h3>
+                <p className="pt-2">Name <span className="ml-16">{baseToken?.name?.split(" ")[0]}</span></p>
+                <p className="pt-2">Symbol <span className="ml-14">{baseToken?.symbol}</span></p>
+                <p className="w-full  pt-2 ">Address <span className=" ml-14">#{baseToken?.address?.slice(0, 4)}</span></p>
+                <button className='absolute right-4 bottom-6 '>
+                    <img src={baseInfo_btn_bg} alt="" />
+                    <img className='ml-4 -mt-10' src={material_symbols} alt="" />
+                </button>
+            </div>
+
+            {/* Quote Token */}
+            <div className=" relative w-[223px] h-[220px]  bg-[#390554] rounded-[10px] text-white px-5 py-4">
+                <h3 className="font-bold text-[18px]">Quote Token </h3>
+                <p className="pt-2">Name <span className="ml-16">{baseToken?.name?.split(" ")[0]}</span></p>
+                <p className="pt-2">Symbol <span className="ml-14">{baseToken?.symbol}</span></p>
+                <p className="w-full  pt-2 ">Address <span className=" ml-14">#{baseToken?.address?.slice(0, 4)}</span></p>
+                <button className='absolute right-4 bottom-6 '>
+                    <img src={baseInfo_btn_bg} alt="" />
+                    <img className='ml-4 -mt-10' src={material_symbols} alt="" />
+                </button>
+            </div>
+            {/* price */}
+            <div className=" relative w-[223px] h-[220px]  bg-[#390554] rounded-[10px] text-white px-5 py-4">
+                <h3 className="font-bold text-[18px]">Price </h3>
+                <p className="pt-2">Price Native <span className="ml-5">ETH {priceNative?.slice(0, 4)}</span></p>
+                <p className="pt-2">Price USD <span className="ml-8">{priceUsd?.slice(0, 4)}m</span></p>
+               
+                <button className='absolute right-4 bottom-6 '>
+                    <img src={baseInfo_btn_bg} alt="" />
+                    <img className='ml-4 -mt-10' src={material_symbols} alt="" />
+                </button>
+            </div>
+
+        </div>
+    );
+};
+
+export default NFTrow;
